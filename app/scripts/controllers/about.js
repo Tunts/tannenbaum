8a33e63f6bbab1ba4e84e9e9a167a469be131458
Larissa Barabasz <lari.barabasz@gmail.com>
2016-04-07 14:27:22 -0300

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xmastreeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xmastreeApp
+ */
+angular.module('xmastreeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xmastreeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xmastreeApp
+ */
+angular.module('xmastreeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
